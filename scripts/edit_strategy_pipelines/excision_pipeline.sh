b47152d72adc0e10d0296416f8531f46b92d0ee3
gramey02 <gramey02@gpudev1.wynton.ucsf.edu>
2026-03-18 11:31:03 -0700

    updated relative file paths

--- scripts/edit_strategy_pipelines/excision_pipeline.sh
+++ scripts/edit_strategy_pipelines/excision_pipeline.sh
@@ -5,31 +5,40 @@
 #$ -o /wynton/protected/home/capra/gramey02/ConklinCollab/scripts/out/excision_pipeline.out
 #$ -e /wynton/protected/home/capra/gramey02/ConklinCollab/scripts/err/excision_pipeline.err
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+project_root="$(cd "$script_dir/../.." && pwd)"
+
 # parse input arguments
-param_file=$2
-source $param_file
-output_dir=$1
+param_file="$2"
+source "$param_file"
+output_dir="$1"
+
+if [[ "$OUTPUT_DIR" = /* ]]; then
+  resolved_output_base="$OUTPUT_DIR"
+else
+  resolved_output_base="$project_root/$OUTPUT_DIR"
+fi
 
 # set exon file
 #exon_file=$EXON_FILE_FOR_ANALYSIS
 # exon_file="/wynton/protected/home/capra/gramey02/ConklinCollab/data/dHS_and_related_GeneSets/Original_GeneSets/2025_04_22/filtered_exon_info/dHS_exonInfo_GTExCurated_transcripts_TPM1.0_ExprProp0.01_2025_04_22.csv"
 #exon_file="/wynton/protected/home/capra/gramey02/ConklinCollab/data/dHS_and_related_GeneSets/Original_GeneSets/2025_04_22/filtered_exon_info/donor_test_exon.csv"
-exon_file=$EXON_FILE_FOR_ANALYSIS
+exon_file="$EXON_FILE_FOR_ANALYSIS"
 
 # script to generate appropriate coordinates to look for excision variants in
-find_variant_region="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/pipeline_scripts/find_variant_region.py"
-python3 $find_variant_region --exon_file $exon_file --output_dir $output_dir"/CommonVars" --upstream_excision_dist $UPSTREAM_EXCISION_DIST --downstream_excision_dist $DOWNSTREAM_EXCISION_DIST --nearest_gene_file $GENE_BODY_FILE --filter_out_nearby_genes $FILTER_OUT_NEARBY_GENES
+find_variant_region="$project_root/scripts/format_variants/find_variant_region.py"
+python3 "$find_variant_region" --exon_file "$exon_file" --output_dir "$output_dir/CommonVars" --upstream_excision_dist "$UPSTREAM_EXCISION_DIST" --downstream_excision_dist "$DOWNSTREAM_EXCISION_DIST" --nearest_gene_file "$GENE_BODY_FILE" --filter_out_nearby_genes "$FILTER_OUT_NEARBY_GENES"
 
 # script to generate list of variants in excision windows to run through excavate
-find_commonVars="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/pipeline_scripts/find_excision_commonVars.sh"
+find_commonVars="$project_root/scripts/get_common_vars/excision/find_excision_commonVars.sh"
 gene_info="$output_dir/CommonVars/gene_filtered_excision_coords.txt"
 echo "Started extracting common var info..."
-qsub -sync y -l mem_free=1G -l h_rt=00:45:00 $find_commonVars "$output_dir/CommonVars" "$param_file" "$gene_info" "$exon_file"
+qsub -sync y -l mem_free=1G -l h_rt=00:45:00 "$find_commonVars" "$output_dir/CommonVars" "$param_file" "$gene_info" "$exon_file"
 echo "Finished extracting common variant info."
 
 # get number of genes that have common vars in excision_window --> make sure there are 2 or more, since that's the minimum required for excision
 common_var_genes=$output_dir"/CommonVars/CommonVars_VarNumOver2_summary_noIDX.txt"
-awk -F'\t' '$2+0 >= 2' $output_dir"/CommonVars/CommonVars_ALL_summary_noIDX.txt" > $common_var_genes
+awk -F'\t' '$2+0 >= 2' "$output_dir/CommonVars/CommonVars_ALL_summary_noIDX.txt" > "$common_var_genes"
 num_common_var_genes=$(wc -l < $common_var_genes) # get the number of genes that have common vars in them (from summary file)
 # I think the filtering in the next step is better
 
@@ -44,39 +53,39 @@ echo "Finished filtering excision snps."
 
 
 # script to generate text files that we'll use to filter vcfs (which we'll then input into EXCAVATE)
-generate_variant_textFiles="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/generate_variant_textFiles.py"
+generate_variant_textFiles="$project_root/scripts/format_variants/generate_variant_textFiles.py"
 echo "Started generating common var loc files..."
 cv_dict_filepath=$output_dir"/CommonVars/CommonVars_ALL_dict.pkl"
-python3 $generate_variant_textFiles --cv_dict_filepath $cv_dict_filepath --exon_file $exon_file --output_dir $output_dir --edit_strat "excision"
+python3 "$generate_variant_textFiles" --cv_dict_filepath "$cv_dict_filepath" --exon_file "$exon_file" --output_dir "$output_dir" --edit_strat "excision"
 num_common_var_genes=$(ls -1 $output_dir"/excavate/CommonVar_locs" | wc -l)
 echo "Finished generating common var loc files."
 
 # script to filter vcfs accordingly
-excavate_vcf_creation="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/generate_filtered_vcfs.sh" # first we need to generate vcf.gz files for the genes that have variants in their ubiquitous regions
+excavate_vcf_creation="$project_root/scripts/format_variants/generate_filtered_vcfs.sh" # first we need to generate vcf.gz files for the genes that have variants in their ubiquitous regions
 input_metadata=$output_dir"/excavate/input_metadata/excavate_run_metadata.txt"
 echo "Started creating vcf files for excavate input..."
-qsub -t 1-"$num_common_var_genes" -l mem_free=2G -l h_rt=01:00:00 -sync y $excavate_vcf_creation "$output_dir" "$param_file" "$input_metadata"
+qsub -t 1-"$num_common_var_genes" -l mem_free=2G -l h_rt=01:00:00 -sync y "$excavate_vcf_creation" "$output_dir" "$param_file" "$input_metadata"
 echo "Finished creating excavate inputs."
 
 # script to run excavate
-run_excavate_script="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/excavate/run_excavate.sh"
+run_excavate_script="$project_root/scripts/EXCAVATE_HT_run/run_excavate.sh"
 echo "Started running EXCAVATE..."
-qsub -t 1-"$num_common_var_genes" -l mem_free=1G -l h_rt=00:45:00 -sync y $run_excavate_script "$output_dir" "$param_file" "$input_metadata"
+qsub -t 1-"$num_common_var_genes" -l mem_free=1G -l h_rt=00:45:00 -sync y "$run_excavate_script" "$output_dir" "$param_file" "$input_metadata"
 echo "Finished running EXCAVATE."
 
 # generate text files for the valid guides so you can filter the individuals' vcfs accordingly
-generate_guide_textFiles="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/generate_guide_textFiles.py"
+generate_guide_textFiles="$project_root/scripts/format_variants/generate_guide_textFiles.py"
 guides_filepath=$output_dir"/excavate/excavate_outputs"
 echo "Started generating position files for valid guides..."
-python3 $generate_guide_textFiles --guides_filepath $guides_filepath --exon_file $exon_file --output_dir $output_dir
+python3 "$generate_guide_textFiles" --guides_filepath "$guides_filepath" --exon_file "$exon_file" --output_dir "$output_dir"
 echo "Finished generating position files for valid guides."
 
 # filter vcfs based on these viable EXCAVATE guides to see how many heterozygous individuals they will capture
-guide_based_filtering="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/bcftools_examples/position_filtering.sh"
+guide_based_filtering="$project_root/scripts/format_variants/position_filtering.sh"
 genes_w_guides=$output_dir"/excavate/het_individuals/metadata/genes_w_valid_guides.txt"
 num_genes_w_guides=$(awk -F'\t' '$1 != "" {n++} END{print n}' $genes_w_guides)
 echo "Started filtering vcfs based on valid guides..."
-qsub -t 1-"$num_genes_w_guides" -l mem_free=3G -l h_rt=02:00:00 -sync y $guide_based_filtering "$output_dir" "$param_file" "$genes_w_guides"
+qsub -t 1-"$num_genes_w_guides" -l mem_free=3G -l h_rt=02:00:00 -sync y "$guide_based_filtering" "$output_dir" "$param_file" "$genes_w_guides"
 echo "Finished filtering vcfs based on valid guides."
 
 # script to calculate number of heterozygous individuals that will be hit by pairs of snps (also runs greedy algorithm)
@@ -93,9 +102,9 @@ heterozygote_directory="$output_dir/excavate/het_individuals/"
 python3 $combine_data_script --heterozygote_directory $heterozygote_directory
 
 # script to get the number of individuals haplotype-specific guides per gene
-get_guide_info="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/pipeline_scripts/get_guides_excision.sh"
+get_guide_info="$project_root/scripts/get_guides/excision_guides.sh"
 echo "Calculating number of guides to target heterozygotes..."
-qsub -t 1-"$num_genes_w_guides" -l mem_free=2G -l h_rt=03:00:00 -sync y $get_guide_info "$output_dir/excavate/guide_numbers" "$param_file" "$genes_w_guides" "$filtered_vcf_dir" "$exon_file"
+qsub -t 1-"$num_genes_w_guides" -l mem_free=2G -l h_rt=03:00:00 -sync y "$get_guide_info" "$output_dir/excavate/guide_numbers" "$param_file" "$genes_w_guides" "$filtered_vcf_dir" "$exon_file"
 echo "Finished calculating number of guides."
 
 # script to combine the haplotype-specific guide information into a single file across genes
--- scripts/edit_strategy_pipelines/indel_pipeline.sh
+++ scripts/edit_strategy_pipelines/indel_pipeline.sh
@@ -5,33 +5,42 @@
 #$ -o /wynton/home/capra/gramey02/ConklinCollab/scripts/out/indel_pipeline.out
 #$ -e /wynton/home/capra/gramey02/ConklinCollab/scripts/err/indel_pipeline.err
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+project_root="$(cd "$script_dir/../.." && pwd)"
+
 # parse input arguments
-param_file=$2
-source $param_file
-output_dir=$1
+param_file="$2"
+source "$param_file"
+output_dir="$1"
+
+if [[ "$OUTPUT_DIR" = /* ]]; then
+  resolved_output_base="$OUTPUT_DIR"
+else
+  resolved_output_base="$project_root/$OUTPUT_DIR"
+fi
 
 # find exon_file
 #exon_file=$EXON_FILE_FOR_ANALYSIS
 #exon_file="/wynton/home/capra/gramey02/ConklinCollab/data/dHS_and_related_GeneSets/Original_GeneSets/2025_04_22/filtered_exon_info/test_exon.csv"
 #exon_file="/wynton/home/capra/gramey02/ConklinCollab/data/dHS_and_related_GeneSets/Original_GeneSets/2025_04_22/filtered_exon_info/dHS_exonInfo_GTExCurated_transcripts_TPM1.0_ExprProp0.01_2025_04_22.csv"
-exon_file=$EXON_FILE_FOR_ANALYSIS
+exon_file="$EXON_FILE_FOR_ANALYSIS"
 
 # get num chromosomes
-col=$(head -1 $exon_file | tr ',' '\n' | grep -nx "chromosome_name" | cut -d: -f1)
-cut -d',' -f"$col" $exon_file | tail -n +2 | sort -u > $OUTPUT_DIR"/"$RUN_NAME"/chromosomes/chrom_set.txt"
-num_chroms=$(wc -l < $OUTPUT_DIR"/"$RUN_NAME"/chromosomes/chrom_set.txt")
+col=$(head -1 "$exon_file" | tr ',' '\n' | grep -nx "chromosome_name" | cut -d: -f1)
+cut -d',' -f"$col" "$exon_file" | tail -n +2 | sort -u > "$resolved_output_base$RUN_NAME/chromosomes/chrom_set.txt"
+num_chroms=$(wc -l < "$resolved_output_base$RUN_NAME/chromosomes/chrom_set.txt")
 
 # script to get ubiquitous exonic regions & identify common vars in them
-ubiq_regions_CommonVar_script="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_CommonVars/get_common_vars_UER.sh"
+ubiq_regions_CommonVar_script="$project_root/scripts/get_common_vars/exon_disruption/get_common_vars_UER.sh"
 echo "Started identifying common coding vars..."
-qsub -l mem_free=1G -l h_rt=00:20:00 -t 1-"$num_chroms" -sync y $ubiq_regions_CommonVar_script "$output_dir" "$param_file" "$num_chroms" "$exon_file"
+qsub -l mem_free=1G -l h_rt=00:20:00 -t 1-"$num_chroms" -sync y "$ubiq_regions_CommonVar_script" "$output_dir" "$param_file" "$num_chroms" "$exon_file"
 echo "Finished identifiying common coding vars."
 
 ##### NMD script goes here
 targetable_common_var_dict="$output_dir/ubiq_region_CommonVars/CommonVars_ALL_dict.pkl"
-NMD_script="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_NMD/Annot_NMD_escape.sh"
+NMD_script="$project_root/scripts/NMD/Annot_NMD_escape.sh"
 echo "Started running NMD analysis on guide-filtered indel vars..."
-qsub -l mem_free=1G -l h_rt=01:00:00 -sync y $NMD_script "$output_dir" "$param_file" "$targetable_common_var_dict"
+qsub -l mem_free=1G -l h_rt=01:00:00 -sync y "$NMD_script" "$output_dir" "$param_file" "$targetable_common_var_dict"
 echo "Finished running NMD analysis."
 common_var_genes=$output_dir"/excavate/input_metadata/excavate_run_metadata.txt"
 num_common_var_genes=$(wc -l < $common_var_genes) # get the number of genes that have common vars in them (from summary file)
@@ -51,120 +60,45 @@ num_common_var_genes=$(wc -l < $common_var_genes) # get the number of genes that
 #------------------------------------------------------------------------------------------------------------------------------
 
 # script to filter vcfs accordingly
-excavate_vcf_creation="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/generate_filtered_vcfs.sh" # first we need to generate vcf.gz files for the genes that have variants in their ubiquitous exonic regions
+excavate_vcf_creation="$project_root/scripts/format_variants/generate_filtered_vcfs.sh" # first we need to generate vcf.gz files for the genes that have variants in their ubiquitous exonic regions
 input_metadata=$output_dir"/excavate/input_metadata/excavate_run_metadata.txt"
 echo "Started creating vcf files for excavate input..."
-qsub -t 1-"$num_common_var_genes" -l mem_free=2G -l h_rt=01:00:00 -sync y $excavate_vcf_creation "$output_dir" "$param_file" "$input_metadata"
+qsub -t 1-"$num_common_var_genes" -l mem_free=2G -l h_rt=01:00:00 -sync y "$excavate_vcf_creation" "$output_dir" "$param_file" "$input_metadata"
 echo "Finished creating excavate inputs."
 
 # script to run excavate
-run_excavate_script="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/excavate/run_excavate.sh"
+run_excavate_script="$project_root/scripts/EXCAVATE_HT_run/run_excavate.sh"
 echo "Started running EXCAVATE..."
-qsub -t 1-"$num_common_var_genes" -l mem_free=1G -l h_rt=00:45:00 -sync y $run_excavate_script "$output_dir" "$param_file" "$input_metadata"
+qsub -t 1-"$num_common_var_genes" -l mem_free=1G -l h_rt=00:45:00 -sync y "$run_excavate_script" "$output_dir" "$param_file" "$input_metadata"
 echo "Finished running EXCAVATE."
 
 # generate text files for the valid guides so you can filter the vcfs accordingly
-generate_guide_textFiles="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_PAMsites/generate_guide_textFiles.py"
+generate_guide_textFiles="$project_root/scripts/format_variants/generate_guide_textFiles.py"
 guides_filepath=$output_dir"/excavate/excavate_outputs" # switch to not include indels later
 echo "Started generating position files for valid guides..."
-python3 $generate_guide_textFiles --guides_filepath $guides_filepath --exon_file $exon_file --output_dir $output_dir
+python3 "$generate_guide_textFiles" --guides_filepath "$guides_filepath" --exon_file "$exon_file" --output_dir "$output_dir"
 echo "Finished generating position files for valid guides."
 
 # filter vcfs based on these viable EXCAVATE guides to see how many heterozygous individuals they will capture
-guide_based_filtering="/wynton/home/capra/gramey02/ConklinCollab/scripts/bcftools_examples/position_filtering.sh"
+guide_based_filtering="$project_root/scripts/format_variants/position_filtering.sh"
 genes_w_guides=$output_dir"/excavate/het_individuals/metadata/genes_w_valid_guides.txt"
 num_genes_w_guides=$(awk -F'\t' '$1 != "" {n++} END{print n}' $genes_w_guides)
 echo "Started filtering vcfs based on valid guides..."
-qsub -t 1-"$num_genes_w_guides" -l mem_free=2G -l h_rt=01:00:00 -sync y $guide_based_filtering "$output_dir" "$param_file" "$genes_w_guides"
+qsub -t 1-"$num_genes_w_guides" -l mem_free=2G -l h_rt=01:00:00 -sync y "$guide_based_filtering" "$output_dir" "$param_file" "$genes_w_guides"
 echo "Finished filtering vcfs based on valid guides."
 
 # script to calculate number of heterozygous individuals for each gene
-get_targeted_hets="/wynton/home/capra/gramey02/ConklinCollab/scripts/DN_targeted_hets/get_targeted_hets.sh"
+get_targeted_hets="$project_root/scripts/get_hets/get_targeted_hets.sh"
 echo "Started calculating heterozygous indvidual numbers..."
 excavate_output_dir=$output_dir"/excavate/excavate_outputs"
 filtered_vcf_dir=$output_dir"/excavate/Guide_filtered_vcfs"
-qsub -l mem_free=2G -l h_rt=01:00:00 -sync y $get_targeted_hets "$output_dir/excavate/het_individuals" "$param_file" "$genes_w_guides" "$excavate_output_dir" "$filtered_vcf_dir"
+qsub -l mem_free=2G -l h_rt=01:00:00 -sync y "$get_targeted_hets" "$output_dir/excavate/het_individuals" "$param_file" "$genes_w_guides" "$excavate_output_dir" "$filtered_vcf_dir"
 echo "Finished calculating heterozygous individual numbers."
 
 # script to calculate number of guides needed to reach heterozygous individuals for each gene
-get_guide_info="/wynton/home/capra/gramey02/ConklinCollab/scripts/pipeline_scripts/get_guides.sh"
+get_guide_info="$project_root/scripts/get_guides/non_excision_guides.sh"
 echo "Calculating number of guides to target heterozygotes..."
 excavate_output_dir=$output_dir"/excavate/excavate_outputs"
 filtered_vcf_dir=$output_dir"/excavate/Guide_filtered_vcfs"
-qsub -l mem_free=2G -l h_rt=01:00:00 -sync y $get_guide_info "$output_dir/excavate/guide_numbers" "$param_file" "$genes_w_guides" "$excavate_output_dir" "$filtered_vcf_dir"
+qsub -l mem_free=2G -l h_rt=01:00:00 -sync y "$get_guide_info" "$output_dir/excavate/guide_numbers" "$param_file" "$genes_w_guides" "$excavate_output_dir" "$filtered_vcf_dir"
 echo "Finished calculating number of guides."
-
-
-
-
-
-
-#-----------------code to generate multiple sets of variants----------------
-# # find common vars slightly differently if we're using shared regions among transcripts or if we want to consider all common vars. Store each created set in an array
-# variant_sets=()
-# summary_file_sets=()
-# branch_names=()
-# sub_branch_names=()
-
-# if (( $USE_ALL_REGIONS == 1 )); then
-#     # script to get common vars across transcripts, no matter the ubiquitous regions
-#     CommonVar_script="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_CommonVars/get_common_vars_tp.sh"
-#     echo "Started identifying common coding vars across all transcript exons..."
-#     qsub -l mem_free=1G -l h_rt=00:30:00 -t 1-"$num_chroms" -sync y $CommonVar_script "$output_dir/all_regions" "$param_file" "$num_chroms" "$exon_file"
-#     echo "Finished identifying common coding vars across transcripts."
-
-#     # add to variant set array
-#     # need to change these to have top level file name in there
-#     variant_sets+=( "$output_dir/all_regions/all_region_CommonVars/CommonVars_ALL_dict.pkl" )
-#     summary_file_sets+=( "$output_dir/all_regions/all_region_CommonVars/CommonVars_ALL_summary_noIDX.txt" )
-#     branch_names+=( "$output_dir/all_regions" )
-#     sub_branch_names+=( "$output_dir/all_regions/all_region_CommonVars")
-# fi
-
-# if (( $USE_UBIQUITOUS_REGIONS == 1 )); then
-#     # script to get ubiquitous exonic regions & identify common vars in them
-#     ubiq_regions_CommonVar_script="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_CommonVars/get_common_vars_UER.sh" # UER = ubiquitous exonic regions
-#     echo "Started identifying common coding vars in ubiquitous exonic regions..."
-#     qsub -l mem_free=1G -l h_rt=00:20:00 -t 1-"$num_chroms" -sync y $ubiq_regions_CommonVar_script "$output_dir/ubiq_regions" "$param_file" "$num_chroms" "$exon_file"
-#     echo "Finished identifying common coding vars in ubiquitous regions."
-
-#     # add to variant set array
-#     # need to change these to have top level file name in thre (ubiq_regions/ubiq_region_CommonVars)
-#     variant_sets+=( "$output_dir/ubiq_regions/ubiq_region_CommonVars/CommonVars_ALL_dict.pkl" )
-#     summary_file_sets+=( "$output_dir/ubiq_regions/ubiq_region_CommonVars/CommonVars_ALL_summary_noIDX.txt" )
-#     branch_names+=( "$output_dir/ubiq_regions" )
-#     sub_branch_names+=( "$output_dir/ubiq_regions/ubiq_region_CommonVars")
-
-# fi
-
-
-# function runs downstream analyses
-
-# for i in "${!variant_sets[@]}"; do
-#     variant_set="${variant_sets[$i]}"
-#     summary_file="${summary_file_sets[$i]}"
-#     branch_name="${branch_names[$i]}"
-#     sub_branch_name="${sub_branch_names[$i]}"
-
-#     # script to run downstream analysis
-#     downstream_analysis_script="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/pipeline_scripts/run_downstream_analysis_indels.sh"
-    
-#     # input and output files
-#     var_set_identifier=$(basename "$branch_name")
-#     job_output="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/out/run_downstream_analysis_indels_${var_set_identifier}.out"
-#     job_err="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/out/run_downstream_analysis_indels_${var_set_identifier}.err"
-
-#     qsub -l mem_free=1G -l h_rt=03:00:00 \
-#         -o $job_output \
-#         -e $job_err \
-#         $downstream_analysis_script \
-#         "$variant_set" "$summary_file" "$branch_name" "$sub_branch_name" "$param_file"
-# done 
-
-## Old NMD code
-# # script to run NMD pipeline on these guide-filtered common vars
-# NMD_script="/wynton/protected/home/capra/gramey02/ConklinCollab/scripts/DN_NMD/Annot_NMD_escape.sh"
-# targetable_common_var_dict="$output_dir/excavate/het_individuals/metadata/targetable_SNP_locs_by_gene.pkl"
-# echo "Started running NMD analysis on guide-filtered indel vars..."
-# qsub -l mem_free=1G -l h_rt=01:00:00 -sync y $NMD_script "$output_dir/NMD" "$param_file" "$targetable_common_var_dict"
-# echo "Finished running NMD analysis."
