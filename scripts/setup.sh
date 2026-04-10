6aaa860b9f77128a11ad54c0db8a02f2c02e27a7
gramey02 <gramey02@gpudev1.wynton.ucsf.edu>
2026-04-10 13:37:51 -0700

    updated setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,13 +2,13 @@
 
 SEARCH1="Grace.Ramey@ucsf.edu"
 REPLACE1="<your_email_here>"
-DIR="<dnd_project_filepath_here>"
+DIR="<dnd_project_directory>"
 
 find "$DIR" -type f -exec sed -i "s|$SEARCH1|$REPLACE1|g" {} +
 echo "Done replacing '$SEARCH1' with '$REPLACE1' in $DIR"
 
 SEARCH2="/wynton/home/capra/gramey02/dnd_project"
-REPLACE2="<absolute_dnd_project_filepath"
+REPLACE2="<dnd_project_directory>"
 
 find "$DIR" -type f -exec sed -i "s|$SEARCH2|$REPLACE2|g" {} +
 echo "Done replacing '$SEARCH2' with '$REPLACE2' in $DIR"
